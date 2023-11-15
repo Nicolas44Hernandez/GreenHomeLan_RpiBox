9f98aa13b951b44ea25fa5721a29efd62ecb18da
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-11-15 15:55:32 +0100

    WIP: log alarm received

--- server_box/server/orchestrator/requests/service.py
+++ server_box/server/orchestrator/requests/service.py
@@ -20,12 +20,16 @@ class OrchestratorRequests:
     """OrchestratorRequests service"""
 
     # Attributes
-    def init_requests_module(self, mqtt_alarm_notif_topic: str, mqtt_command_topic: str):
+    def init_requests_module(
+        self, mqtt_alarm_notif_topic: str, mqtt_command_topic: str
+    ):
         """Initialize the requests callbacks for the orchestrator"""
         logger.info("initializing Orchestrator requests module")
 
         # # Set callback functions
-        thread_manager_service.set_msg_reception_callback(self.thread_msg_reception_callback)
+        thread_manager_service.set_msg_reception_callback(
+            self.thread_msg_reception_callback
+        )
 
         # Subscribe to command reception MQTT topic
         logger.info(f"Subscribe to MQTT topic: {mqtt_command_topic}")
@@ -50,13 +54,14 @@ class OrchestratorRequests:
         # Thread message is an alarm
         if msg.startswith("al"):
             _device, _type = msg.split("_")[1:]
+            logger.inf(f"alarm received device:{_device}  type:{_type}")
             if _type == "db":
                 alarm_type = "doorbell"
             elif _type == "pd":
                 alarm_type = "presence"
             elif _type == "em":
                 alarm_type = "emergency_btn"
-            elif _type == "bat": #al_bt1_bat
+            elif _type == "bat":  # al_bt1_bat
                 alarm_type = f"battery_btn_{_device}"
             else:
                 logger.error(f"Error in alarm received format {msg}")
@@ -68,9 +73,7 @@ class OrchestratorRequests:
                 wifi_bands_manager_service.set_band_status(band="5GHz", status=True)
 
             # Transfer alarm to cloud server and liveobjects
-            orchestrator_notification_service.transfer_alarm_to_cloud_server(
-                alarm_type
-            )
+            orchestrator_notification_service.transfer_alarm_to_cloud_server(alarm_type)
 
         # If Thread message is buttons a command
         elif msg.startswith("cmd"):
@@ -82,9 +85,7 @@ class OrchestratorRequests:
             device, level = msg.split("_")[1:]
             logger.info(f"Device {device} battery level received: {level}")
             orchestrator_notification_service.transfer_device_battery_level_to_cloud_server(
-                device_type=device_type,
-                device=device,
-                batLevel=level
+                device_type=device_type, device=device, batLevel=level
             )
 
         # If Thread message is a direct wifi command
@@ -99,16 +100,16 @@ class OrchestratorRequests:
                     if band == "all":
                         wifi_bands_manager_service.set_wifi_status(status=status)
                     else:
-                        wifi_bands_manager_service.set_band_status(band=band, status=status)
+                        wifi_bands_manager_service.set_band_status(
+                            band=band, status=status
+                        )
             except:
                 logger.error(f"Error in message received format {msg}")
 
     def alarm_notification_reception_callback(self, msg):
         """Callback for MQTT object alarm notification"""
         logger.info(f"Alarm notification received: {msg} ")
-        orchestrator_notification_service.transfer_alarm_to_cloud_server(
-            msg["type"]
-        )
+        orchestrator_notification_service.transfer_alarm_to_cloud_server(msg["type"])
 
     def command_reception_callback(self, msg):
         """Callback for MQTT command reception"""
@@ -117,12 +118,15 @@ class OrchestratorRequests:
             msg = msg["command"]
         try:
             command_number = int(msg.split("cmd_")[1])
-            if not orchestrator_commands_service.execute_predefined_command(command_number):
+            if not orchestrator_commands_service.execute_predefined_command(
+                command_number
+            ):
                 logger.error("Error in command format")
                 return
         except:
             logger.error("Error in command format")
         logger.info("Command executed")
+
 
 orchestrator_requests_service: OrchestratorRequests = OrchestratorRequests()
 """ OrchestratorRequests service singleton"""
