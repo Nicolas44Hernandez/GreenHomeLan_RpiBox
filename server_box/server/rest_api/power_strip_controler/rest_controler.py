fee743f46dc9533a365f5a50c9656ef7384ebfb6
Nicolas Hernandez <n44hernandezp@gmail.com>
2024-06-20 10:29:59 +0200

    wip: single relay set status api

--- server_box/server/rest_api/power_strip_controler/rest_controler.py
+++ server_box/server/rest_api/power_strip_controler/rest_controler.py
@@ -69,7 +69,7 @@ class PowerStripStatusApi(MethodView):
 
 
 @bp.route("/<relay>")
-class WifiBandsStatusApi(MethodView):
+class SingleRelayGetStatusApi(MethodView):
     """API to retrieve single relay status"""
 
     @box_sleeping
@@ -86,6 +86,10 @@ class WifiBandsStatusApi(MethodView):
         status = power_strip_manager_service.get_single_relay_status(relay_number=int(relay))
         return status
 
+
+@bp.route("/single")
+class SingleRelaySetStatusApi(MethodView):
+    """API to retrieve single relay status"""
     @box_sleeping
     @bp.doc(responses={400: "BAD_REQUEST"})
     @bp.arguments(SingleRelayStatusSchema, location="query")
